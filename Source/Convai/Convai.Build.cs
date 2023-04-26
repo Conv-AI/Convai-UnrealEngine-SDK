d4067733537144a86d16181b5f09fd917052a8c7
Convai-Unreal-Dev <convai.unreal@gmail.com>
2023-04-26 21:24:55 +0200

    Updated build.cs to include Android gRPC libs

--- Source/Convai/Convai.Build.cs
+++ Source/Convai/Convai.Build.cs
@@ -1,14 +1,18 @@
 // Copyright 2022 Convai Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
 
 
 
 public class Convai : ModuleRules
 {
-	
-	private string ModulePath
+    private static ConvaiPlatform ConvaiPlatformInstance;
+
+    private string ModulePath
 	{
 		get { return ModuleDirectory; }
 	}
@@ -17,52 +21,44 @@ public class Convai : ModuleRules
 	{
 		get { return Path.GetFullPath(Path.Combine(ModulePath, "../ThirdParty/")); }
 	}
-	
-	private string AudioDecoderLibraryPath
-    {
-		get
-		{
-			return Path.GetFullPath(Path.Combine(ThirdPartyPath, "AudioDecoder", "Lib"));
-		}
-	}
 
-    private string gRPCLibraryPath
+    private ConvaiPlatform GetConvaiPlatformInstance(ReadOnlyTargetRules Target)
     {
-       get
-       {
-           return Path.GetFullPath(Path.Combine(ThirdPartyPath, "gRPC", "gRPC", "lib"));
-       }
+        var ConvaiPlatformType = System.Type.GetType("ConvaiPlatform_" + Target.Platform.ToString());
+        if (ConvaiPlatformType == null)
+        {
+            throw new BuildException("Convai does not support platform " + Target.Platform.ToString());
+        }
+
+        var PlatformInstance = Activator.CreateInstance(ConvaiPlatformType) as ConvaiPlatform;
+        if (PlatformInstance == null)
+        {
+            throw new BuildException("Convai could not instantiate platform " + Target.Platform.ToString());
+        }
+
+        return PlatformInstance;
     }
 
-    private string ProtobufLibraryPath
+    private bool ConfigurePlatform(string Platform, UnrealTargetConfiguration Configuration)
     {
-       get
-       {
-           return Path.GetFullPath(Path.Combine(ThirdPartyPath, "gRPC", "protobuf", "lib"));
-       }
+        //Convai thirdparty libraries root path
+        string root = ThirdPartyPath;
+        foreach (var arch in ConvaiPlatformInstance.Architectures())
+        {
+            string fullPath = root + "grpc/" + "lib/" + ConvaiPlatformInstance.LibrariesPath + arch;
+            PublicAdditionalLibraries.AddRange(Directory.GetFiles(fullPath));
+            // foreach (var a in PublicAdditionalLibraries)
+            //     Console.WriteLine(a);
+        }
+        return false;
     }
-
-    private string zlibLibraryPath
-    {
-       get
-       {
-           return Path.GetFullPath(Path.Combine(ThirdPartyPath, "zlib"));
-       }
-    }
-
-
-	// private string CPRLibraryPath
-    // {
-	// 	get
-	// 	{
-	// 		return Path.GetFullPath(Path.Combine(ThirdPartyPath, "cpr", "Lib"));
-	// 	}
-	// }
 
     public Convai(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		bUsePrecompiled = true;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        ConvaiPlatformInstance = GetConvaiPlatformInstance(Target);
+        bUsePrecompiled = true;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", 
 			"CoreUObject",
@@ -81,7 +77,15 @@ public class Convai : ModuleRules
             "zlib"
         });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        if (Target.Platform == UnrealTargetPlatform.Android)
+        {
+            PublicDependencyModuleNames.AddRange(new string[] { "ApplicationCore", "AndroidPermission" });
+            string BuildPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
+            AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(BuildPath, "Convai_AndroidAPL.xml"));
+        }
+
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Opus codec thirdparty dependency
 		AddEngineThirdPartyPrivateStaticDependencies(Target, "libOpus");
@@ -101,74 +105,94 @@ public class Convai : ModuleRules
 
         PrivateIncludePaths.AddRange(
             new string[] {
-                    Path.Combine(ThirdPartyPath, "AudioDecoder", "Include"),
-					Path.Combine(ThirdPartyPath, "gRPC", "gRPC", "Include"),
-					//Path.Combine(ThirdPartyPath, "gRPC", "protobuf", "Include"),
-					// Path.Combine(ThirdPartyPath, "cpr", "Include"),
-				// ... add other private include paths required here ...
+					Path.Combine(ThirdPartyPath, "gRPC", "Include"),
 			});
 
-        // Audio decoding/encoding library
-        PublicAdditionalLibraries.Add(Path.Combine(AudioDecoderLibraryPath, "AudioDecoder.lib"));
 
-        // gRPC libraries
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "address_sorting.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "grpc.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "gpr.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "grpc++.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "grpc++_alts.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "grpc++_error_details.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "grpc++_reflection.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "grpcpp_channelz.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "upb.lib"));
-
-        PublicAdditionalLibraries.AddRange(
-            Directory.GetFiles(Path.Combine(gRPCLibraryPath))
-        );
-
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "address_sorting.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "cares.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "gpr.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "grpc_unsecure.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "grpc++_unsecure.lib"));
-
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "grpc++.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "grpcpp_channelz.lib"));
-
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "libprotobuf.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "upb.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_base.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_malloc_internal.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_raw_logging_internal.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_spinlock_wait.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_throw_delegate.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_time.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_time_zone.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_graphcycles_internal.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_synchronization.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_cord.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_str_format_internal.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_strings.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_strings_internal.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_status.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_statusor.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_bad_optional_access.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_stacktrace.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_symbolize.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(gRPCLibraryPath, "absl_int128.lib"));
-
-
-        //      PublicAdditionalLibraries.Add(Path.Combine(ProtobufLibraryPath, "libprotobuf.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(zlibLibraryPath, "zlib.lib"));
-        //PublicAdditionalLibraries.Add(Path.Combine(zlibLibraryPath, "zlibstatic.lib"));
-
-        // curl cpr libraries
-        // PublicAdditionalLibraries.Add(Path.Combine(CPRLibraryPath, "cpr.lib"));
-        // PublicAdditionalLibraries.Add(Path.Combine(CPRLibraryPath, "libcurl.lib"));
 
         //if (Target.Platform == UnrealTargetPlatform.Win64)
         {
 			PrecompileForTargets = PrecompileTargetsType.Any;
 		}
+
+        //ThirdParty Libraries
+        ConfigurePlatform(Target.Platform.ToString(), Target.Configuration);
     }
 }
+
+
+
+public abstract class ConvaiPlatform
+{
+    public virtual string ConfigurationDir(UnrealTargetConfiguration Configuration)
+    {
+        if (Configuration == UnrealTargetConfiguration.Debug || Configuration == UnrealTargetConfiguration.DebugGame)
+        {
+            return "Debug/";
+        }
+        else
+        {
+            return "Release/";
+        }
+    }
+    public abstract string LibrariesPath { get; }
+    public abstract List<string> Architectures();
+    public abstract string LibraryPrefixName { get; }
+    public abstract string LibraryPostfixName { get; }
+}
+
+public class ConvaiPlatform_Win64 : ConvaiPlatform
+{
+    public override string LibrariesPath { get { return "win64/"; } }
+    public override List<string> Architectures() { return new List<string> { "" }; }
+    public override string LibraryPrefixName { get { return ""; } }
+    public override string LibraryPostfixName { get { return ".lib"; } }
+}
+
+public class ConvaiPlatform_Android : ConvaiPlatform
+{
+    public override string LibrariesPath { get { return "android/"; } }
+    public override List<string> Architectures() { return new List<string> { "armeabi-v7a/", "arm64-v8a/", "x86_64/" }; }
+    public override string LibraryPrefixName { get { return "lib"; } }
+    public override string LibraryPostfixName { get { return ".a"; } }
+}
+
+//public class ConvaiPlatform_Linux : ConvaiPlatform
+//{
+//    public override string LibrariesPath { get { return "linux/"; } }
+//    public override List<string> Architectures() { return new List<string> { "" }; }
+//    public override string LibraryPrefixName { get { return "lib"; } }
+//    public override string LibraryPostfixName { get { return ".a"; } }
+//}
+
+//public class ConvaiPlatform_PS5 : ConvaiPlatform
+//{
+//    public override string LibrariesPath { get { return "ps5/"; } }
+//    public override List<string> Architectures() { return new List<string> { "" }; }
+//    public override string LibraryPrefixName { get { return "lib"; } }
+//    public override string LibraryPostfixName { get { return ".a"; } }
+//}
+
+//public class ConvaiPlatform_Mac : ConvaiPlatform
+//{
+//    public override string ConfigurationDir(UnrealTargetConfiguration Configuration)
+//    {
+//        return "";
+//    }
+//    public override string LibrariesPath { get { return "mac/"; } }
+//    public override List<string> Architectures() { return new List<string> { "" }; }
+//    public override string LibraryPrefixName { get { return "lib"; } }
+//    public override string LibraryPostfixName { get { return ".a"; } }
+//}
+
+//public class ConvaiPlatform_IOS : ConvaiPlatform
+//{
+//    public override string ConfigurationDir(UnrealTargetConfiguration Configuration)
+//    {
+//        return "";
+//    }
+//    public override string LibrariesPath { get { return "ios/"; } }
+//    public override List<string> Architectures() { return new List<string> { "" }; }
+//    public override string LibraryPrefixName { get { return "lib"; } }
+//    public override string LibraryPostfixName { get { return ".a"; } }
+//}
