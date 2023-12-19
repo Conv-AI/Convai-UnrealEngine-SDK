deface9929defdea77af7648916fdee8b2d9fb25
mrd-convai <mriganka@convai.com>
2023-12-19 12:14:14 +0000

    Added validation for spaces in API_Key, SessionID and CharacterID

--- Source/Convai/Public/ConvaiUtils.h
+++ Source/Convai/Public/ConvaiUtils.h
@@ -102,6 +102,11 @@ public:
 	{
 		if ((API_Key.Len()))
 		{
+			if (API_Key[0] == TEXT(' ') || API_Key[API_Key.Len() - 1] == TEXT(' ')) 
+			{
+				UE_LOG(ConvaiFormValidationLog,Warning,TEXT("API Key contains space, please enter correct API Key"));
+				return false;
+			}
 			return true;
 		}
 		else
@@ -115,6 +120,11 @@ public:
 	{
 		if ((SessionID.Len()))
 		{
+			if (SessionID[0] == TEXT(' ') || SessionID[SessionID.Len() - 1] == TEXT(' ')) 
+			{
+				UE_LOG(ConvaiFormValidationLog, Warning, TEXT("Session ID contains space, please enter correct Session ID"));
+				return false;
+			}
 			return true;
 		}
 		else
@@ -128,6 +138,11 @@ public:
 	{
 		if ((CharacterID.Len()))
 		{
+			if (CharacterID[0] == TEXT(' ') || CharacterID[CharacterID.Len() - 1] == TEXT(' '))
+			{
+				UE_LOG(ConvaiFormValidationLog, Warning, TEXT("CharacterID Contains space, pleae enter correct Character ID"));
+				return false;
+			}
 			return true;
 		}
 		else
