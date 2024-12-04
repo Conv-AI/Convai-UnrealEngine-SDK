1dc07c4d0a62b7b69c2cf223f0889cbbc936868e
Anmol <anmol@convai.com>
2024-12-04 12:57:32 +0530

    feat(ConvaiEditor) : Add Convai editor module

---
+++ Source/ConvaiEditor/Private/ConvaiEditor.cpp
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ConvaiEditor.h"
+
+#define LOCTEXT_NAMESPACE "FConvaiEditorModule"
+
+void FConvaiEditorModule::StartupModule()
+{
+	
+}
+
+void FConvaiEditorModule::ShutdownModule()
+{
+	
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FConvaiEditorModule, ConvaiEditor)
