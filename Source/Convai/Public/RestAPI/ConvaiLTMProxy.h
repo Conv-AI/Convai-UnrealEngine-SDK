cfc549207326c492b9d84868a22ed12b45be3cc4
Anmol <anmol@convai.com>
2024-12-05 18:05:02 +0530

    feat(LTM) : Add proxy API calls for Create, Update, List, Delete speaker ID

--- Source/Convai/Public/RestAPI/ConvaiLTMProxy.h
+++ Source/Convai/Public/RestAPI/ConvaiLTMProxy.h
@@ -3,15 +3,184 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Net/OnlineBlueprintCallProxyBase.h"
+#include "RestAPI/ConvaiAPIBase.h"
 #include "ConvaiLTMProxy.generated.h"
 
-/**
- * 
- */
+struct FConvaiSpeakerInfo;
+
+DECLARE_LOG_CATEGORY_EXTERN(LTMHttpLogs, Log, All);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FStringHttpResponseCallbackSignature, FString, ResponseString);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSpeakerIDListHttpResponseCallbackSignature, const TArray<FConvaiSpeakerInfo>&, SpeakerIDs);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSpeakerIDHttpResponseCallbackSignature, const FConvaiSpeakerInfo&, SpeakerID);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FLTMStatusHttpResponseCallbackSignature, bool, Status);
+
+
+
+// Create speaker id
 UCLASS()
-class CONVAI_API UConvaiLTMProxy : public UOnlineBlueprintCallProxyBase
+class CONVAI_API UConvaiCreateSpeakerID : public UConvaiAPIBaseProxy
 {
 	GENERATED_BODY()
-	
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FSpeakerIDHttpResponseCallbackSignature OnSuccess;
+
+	UPROPERTY(BlueprintAssignable)
+	FSpeakerIDHttpResponseCallbackSignature OnFailure;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", DisplayName = "Convai Create Speaker ID"), Category = "Convai|LTM")
+	static UConvaiCreateSpeakerID* ConvaiCreateSpeakerIDProxy(FString SpeakerName);
+
+protected:
+	virtual bool ConfigureRequest(TSharedRef<IHttpRequest> Request, const TCHAR* Verb) override;
+	virtual bool AddContentToRequest(TArray<uint8>& DataToSend, const FString& Boundary)  override { return false; }
+	virtual bool AddContentToRequestAsString(TSharedPtr<FJsonObject>& ObjectToSend) override;
+	virtual void HandleSuccess() override;
+	virtual void HandleFailure() override;
+
+	FString AssociatedSpeakerName;
+	FConvaiSpeakerInfo AssociatedSpeakerInfo;
+};
+// END create speaker id
+
+
+
+
+// List speaker ids
+UCLASS()
+class CONVAI_API UConvaiListSpeakerID : public UConvaiAPIBaseProxy
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FSpeakerIDListHttpResponseCallbackSignature OnSuccess;
+
+	UPROPERTY(BlueprintAssignable)
+	FSpeakerIDListHttpResponseCallbackSignature OnFailure;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", DisplayName = "Convai List Speaker IDs"), Category = "Convai|LTM")
+	static UConvaiListSpeakerID* ConvaiListSpeakerIDProxy();
+
+protected:
+	virtual bool ConfigureRequest(TSharedRef<IHttpRequest> Request, const TCHAR* Verb) override;
+	virtual bool AddContentToRequest(TArray<uint8>& DataToSend, const FString& Boundary)  override { return false; }
+	virtual bool AddContentToRequestAsString(TSharedPtr<FJsonObject>& ObjectToSend) override { return false; }
+	virtual void HandleSuccess() override;
+	virtual void HandleFailure() override;
+};
+// END list speaker ids
+
+
+
+
+
+
+// Delete speaker id
+UCLASS()
+class CONVAI_API UConvaiDeleteSpeakerID : public UConvaiAPIBaseProxy
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FStringHttpResponseCallbackSignature OnSuccess;
+
+	UPROPERTY(BlueprintAssignable)
+	FStringHttpResponseCallbackSignature OnFailure;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", DisplayName = "Convai Delete Speaker ID"), Category = "Convai|LTM")
+	static UConvaiDeleteSpeakerID* ConvaiDeleteSpeakerIDProxy(FString SpeakerID);
+
+protected:
+	virtual bool ConfigureRequest(TSharedRef<IHttpRequest> Request, const TCHAR* Verb) override;
+	virtual bool AddContentToRequest(TArray<uint8>& DataToSend, const FString& Boundary)  override { return false; }
+	virtual bool AddContentToRequestAsString(TSharedPtr<FJsonObject>& ObjectToSend) override;
+	virtual void HandleSuccess() override;
+	virtual void HandleFailure() override;
+
+	FString AssociatedSpeakerID;
+};
+// END Delete speaker id
+
+
+
+
+
+
+// Get LTM status
+UCLASS()
+class CONVAI_API UConvaiGetLTMStatus : public UConvaiAPIBaseProxy
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FLTMStatusHttpResponseCallbackSignature OnSuccess;
+
+	UPROPERTY(BlueprintAssignable)
+	FLTMStatusHttpResponseCallbackSignature OnFailure;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", DisplayName = "Convai Get LTM Status"), Category = "Convai|LTM")
+	static UConvaiGetLTMStatus* ConvaiGetLTMStatusProxy(FString CharacterID);
+
+protected:
+	virtual bool ConfigureRequest(TSharedRef<IHttpRequest> Request, const TCHAR* Verb) override;
+	virtual bool AddContentToRequest(TArray<uint8>& DataToSend, const FString& Boundary)  override { return false; }
+	virtual bool AddContentToRequestAsString(TSharedPtr<FJsonObject>& ObjectToSend) override;
+	virtual void HandleSuccess() override;
+	virtual void HandleFailure() override;
+
+	FString AssociatedCharacterID;
+};
+// END Get LTM status
+
+
+
+
+
+
+// Set LTM status
+UCLASS()
+class CONVAI_API UConvaiSetLTMStatus : public UConvaiAPIBaseProxy
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FStringHttpResponseCallbackSignature OnSuccess;
+
+	UPROPERTY(BlueprintAssignable)
+	FStringHttpResponseCallbackSignature OnFailure;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", DisplayName = "Convai Set LTM Status"), Category = "Convai|LTM")
+	static UConvaiSetLTMStatus* ConvaiSetLTMStatusProxy(FString CharacterID, bool bEnable);
+
+protected:
+	virtual bool ConfigureRequest(TSharedRef<IHttpRequest> Request, const TCHAR* Verb) override;
+	virtual bool AddContentToRequest(TArray<uint8>& DataToSend, const FString& Boundary)  override { return false; }
+	virtual bool AddContentToRequestAsString(TSharedPtr<FJsonObject>& ObjectToSend) override;
+	virtual void HandleSuccess() override;
+	virtual void HandleFailure() override;
+
+	FString AssociatedCharacterID;
+	bool bAssociatedEnable;
+};
+// END Set LTM status
+
+
+
+
+
+
+UCLASS()
+class CONVAI_API UConvaiLTMUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	static bool ParseConvaiSpeakerInfoArray(const FString& JsonString, TArray<FConvaiSpeakerInfo>& OutSpeakerInfoArray);
+	static bool GetLTMStatus(const FString& JsonString, bool& bEnabled);
 };
