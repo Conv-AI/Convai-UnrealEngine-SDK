1b883c984f372a0acd6844109810f21723e93003
Anmol <anmol@convai.com>
2024-12-05 10:18:17 +0530

    feat(ConvaiEditor) : Add editor  utility widget base class

---
+++ Source/ConvaiEditor/Public/ConvaiEUWBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorUtilityWidget.h"
+#include "ConvaiEUWBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class CONVAIEDITOR_API UConvaiEUWBase : public UEditorUtilityWidget
+{
+	GENERATED_BODY()
+	
+};
--- Source/ConvaiEditor/Public/ConvaiEditor.h
+++ Source/ConvaiEditor/Public/ConvaiEditor.h
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright 2022 Convai Inc. All Rights Reserved.
 
 #pragma once
 
